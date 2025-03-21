00b445329c69f8d8ffe5d25a6834f5fb174ebc5d
SySJorge <jflores@s-s.mx>
2025-03-21 09:17:04 -0600

    SelectCliente

---
+++ WaPropiedadIntelectual/Controllers/WaClienteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaPropiedadIntelectual.Models.Cliente;
+
+namespace WaPropiedadIntelectual.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaClienteController : ControllerBase
+    {
+        [HttpGet("selectAll")]
+        public IEnumerable<ClienteObject> SelectAll()
+        {
+            return Cliente.SelectAll();
+        }
+    }
+}
