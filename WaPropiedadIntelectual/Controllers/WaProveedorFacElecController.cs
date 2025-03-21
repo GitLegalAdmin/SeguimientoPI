b4a47509c48cdab46db9e55b548ef294443f1eea
SySJorge <jflores@s-s.mx>
2025-03-21 09:14:45 -0600

    SelectApis

---
+++ WaPropiedadIntelectual/Controllers/WaProveedorFacElecController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaPropiedadIntelectual.Models.ProveedorFacElec;
+
+namespace WaPropiedadIntelectual.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaProveedorFacElecController : ControllerBase
+    {
+        [HttpGet("selectAll")]
+        public IEnumerable<ProveedorFacElecObject> SelectAll()
+        {
+            return ProveedorFacElec.SelectAll();
+        }
+    }
+}
---
+++ WaPropiedadIntelectual/Controllers/WaTipoComunicacionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaPropiedadIntelectual.Models.TipoComunicacion;
+
+namespace WaPropiedadIntelectual.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaTipoComunicacionController : ControllerBase
+    {
+        [HttpGet("selectAll")]
+        public IEnumerable<TipoComunicacionObject> SelectAll()
+        {
+            return TipoComunicacion.SelectAll();
+        }
+    }
+}
